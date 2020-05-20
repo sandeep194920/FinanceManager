92f2b11daff7a3739d30dd4f7ac7ac3288a75425
sandeep194920 <sandeepamaranath@gmail.com>
2020-05-19 23:09:43 -0400

    developing modal / dialog

--- src/components/EasySplit/DetailsModal.js
+++ src/components/EasySplit/DetailsModal.js
@@ -2,10 +2,26 @@ import React from "react";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Grid } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    margin: "auto",
+  },
+  dialogContainer: {},
+  detailItem: {
+    marginBottom: "10px",
+  },
+  detailHead: {
+    marginRight: "7em",
+  },
+}));
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -13,10 +29,15 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export default function DetailsModal(props) {
   const { dialogOpen, dialogCloseHandler } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  //   const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
     <React.Fragment>
       <Dialog
+        className={classes.dialog}
         open={dialogOpen}
         TransitionComponent={Transition}
         keepMounted
@@ -25,19 +46,110 @@ export default function DetailsModal(props) {
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogTitle id="alert-dialog-slide-title">
-          Sandeep Amarnath (Sa194920)
+          <Typography
+            color={theme.palette.type === "light" ? "primary" : "secondary"}
+            variant={matchesSM ? "subtitle2" : "body1"}
+          >
+            Sandeep Amarnath (Sa194920)
+          </Typography>
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
+          <Grid
+            className={classes.dialogContainer}
+            container
+            spacing={2}
+            direction="column"
+          >
+            <Grid container item justify="space-between" alignItems="center">
+              <Typography
+                className={classes.detailHead}
+                variant="subtitle2"
+                color="textPrimary"
+              >
+                Transaction Date &nbsp;
+              </Typography>
+              <Typography variant="subtitle2" color="textPrimary">
+                20 Mar 2019
+              </Typography>
+            </Grid>
+
+            <Grid container item justify="space-between" alignItems="center">
+              <Typography
+                className={classes.detailHead}
+                variant="subtitle2"
+                color="textPrimary"
+              >
+                Transaction Amount &nbsp;
+              </Typography>
+              <Typography variant="subtitle2" color="textPrimary">
+                20$
+              </Typography>
+            </Grid>
+
+            <Grid container item justify="space-between" alignItems="center">
+              <Typography
+                className={classes.detailHead}
+                variant="subtitle2"
+                color="textPrimary"
+              >
+                Paid by &nbsp;
+              </Typography>
+              <Typography variant="subtitle2" color="textPrimary">
+                You
+              </Typography>
+            </Grid>
+
+            <Grid container item justify="space-between" alignItems="center">
+              <Typography
+                className={classes.detailHead}
+                variant="subtitle2"
+                color="textPrimary"
+              >
+                Split type &nbsp;
+              </Typography>
+              <Typography variant="subtitle2" color="textPrimary">
+                Equal
+              </Typography>
+            </Grid>
+
+            <Grid container item justify="space-between" alignItems="center">
+              <Typography
+                className={classes.detailHead}
+                variant="subtitle2"
+                color="textPrimary"
+              >
+                Category &nbsp;
+              </Typography>
+              <Typography variant="subtitle2" color="textPrimary">
+                Entertainment
+              </Typography>
+            </Grid>
+
+            <Grid container item justify="space-between" alignItems="center">
+              <Typography
+                className={classes.detailHead}
+                variant="subtitle2"
+                color="textPrimary"
+              >
+                Details &nbsp;
+              </Typography>
+              <Typography variant="subtitle2" color="textPrimary">
+                Watched Starwars movie
+              </Typography>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
-          <Button onClick={dialogCloseHandler} color="primary">
+          <Button
+            onClick={dialogCloseHandler}
+            color={theme.palette.type === "light" ? "primary" : "secondary"}
+          >
             Edit
           </Button>
-          <Button onClick={dialogCloseHandler} color="primary">
+          <Button
+            onClick={dialogCloseHandler}
+            color={theme.palette.type === "light" ? "primary" : "secondary"}
+          >
             Cancel
           </Button>
         </DialogActions>

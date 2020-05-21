57e7fa4c3948b49dee7febdfd14b2828a19e13e3
sandeep194920 <sandeepamaranath@gmail.com>
2020-05-20 23:49:34 -0400

    modified DetailsModal

--- src/components/EasySplit/DetailsModal.js
+++ src/components/EasySplit/DetailsModal.js
@@ -20,13 +20,31 @@ import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined"
 import CallSplitIcon from "@material-ui/icons/CallSplit";
 import CategoryIcon from "@material-ui/icons/Category";
 import DetailsIcon from "@material-ui/icons/Details";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   dialog: {
-    margin: "auto",
+    width: "40%",
+    [theme.breakpoints.down("md")]: {
+      width: "50%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "90%",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+  dialogTitle: {
+    padding: "30px 30px 20px 45px",
   },
   dialogContainer: {
-    // marginBottom: "1em",
+    "&.MuiDialogContent-root": {
+      paddingTop: 0,
+    },
+    [theme.breakpoints.down("xs")]: {
+      padding: "5px 5px",
+    },
   },
   dialogActions: {
     marginBottom: "1em",
@@ -41,8 +59,13 @@ const useStyles = makeStyles((theme) => ({
   listIcon: {
     minWidth: "38px",
   },
-  listItemTextHead: {
-    marginRight: "6em",
+  listItemText: {
+    flex: "inherit",
+    [theme.breakpoints.down("xs")]: {
+      "& .MuiTypography-body1": {
+        fontSize: "0.8em",
+      },
+    },
   },
 }));
 
@@ -55,12 +78,12 @@ export default function DetailsModal(props) {
   const classes = useStyles();
   const theme = useTheme();
   const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
-  //   const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+  // const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
     <React.Fragment>
       <Dialog
-        className={classes.dialog}
+        classes={{ paper: classes.dialog }}
         open={dialogOpen}
         TransitionComponent={Transition}
         keepMounted
@@ -69,129 +92,218 @@ export default function DetailsModal(props) {
         aria-describedby="alert-dialog-slide-description"
       >
         <DialogTitle
-          style={{ marginTop: "1em", marginLeft: "5%" }}
+          disableTypography
+          classes={{ root: classes.dialogTitle }}
           id="alert-dialog-slide-title"
         >
           <Typography
             color={theme.palette.type === "light" ? "primary" : "secondary"}
-            variant={matchesSM ? "subtitle2" : "body1"}
+            variant={matchesSM ? "subtitle2" : "subtitle1"}
           >
             Sandeep Amarnath (Sa194920)
           </Typography>
         </DialogTitle>
-        <DialogContent className={classes.dialogContainer}>
-          <List>
-            <ListItem>
-              <ListItemIcon classes={{ root: classes.listIcon }}>
-                <DateRangeIcon
-                  color={
-                    theme.palette.type === "dark" ? "secondary" : "primary"
-                  }
-                />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ root: classes.listItemTextHead }}
-                primary="Transaction Date"
-              />
-              <ListItemText primary="20 March 2020" />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <ListItemIcon classes={{ root: classes.listIcon }}>
-                <AccountBalanceWalletOutlinedIcon
-                  color={
-                    theme.palette.type === "dark" ? "secondary" : "primary"
-                  }
-                />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ root: classes.listItemTextHead }}
-                style={{ marginRight: "0em" }}
-                primary="Transaction Amount"
-              />
-              <ListItemText primary="100$" />
-            </ListItem>
-            <Divider />
 
-            <ListItem>
-              <ListItemIcon classes={{ root: classes.listIcon }}>
-                <PersonOutlineOutlinedIcon
-                  color={
-                    theme.palette.type === "dark" ? "secondary" : "primary"
-                  }
-                />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ root: classes.listItemTextHead }}
-                // style={{ marginRight: "0em" }}
-                primary="Paid by"
-              />
-              <ListItemText primary="You" />
-            </ListItem>
-            <Divider />
+        <Grid container>
+          <DialogContent classes={{ root: classes.dialogContainer }}>
+            <List>
+              <ListItem>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item style={{ display: "inherit" }}>
+                    <ListItemIcon classes={{ root: classes.listIcon }}>
+                      <DateRangeIcon
+                        fontSize={matchesSM ? "small" : "default"}
+                        color={
+                          theme.palette.type === "dark"
+                            ? "secondary"
+                            : "primary"
+                        }
+                      />
+                    </ListItemIcon>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      primary={matchesSM ? "Date" : "Transaction Date"}
+                    />
+                  </Grid>
+                  <Grid>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      primary="20 March 2020"
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider />
 
-            <ListItem>
-              <ListItemIcon classes={{ root: classes.listIcon }}>
-                <CallSplitIcon
-                  color={
-                    theme.palette.type === "dark" ? "secondary" : "primary"
-                  }
-                />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ root: classes.listItemTextHead }}
-                style={{ marginRight: "8em" }}
-                primary="Split"
-              />
-              <ListItemText primary="Equal" />
-            </ListItem>
-            <Divider />
+              <ListItem>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item style={{ display: "inherit" }}>
+                    <ListItemIcon classes={{ root: classes.listIcon }}>
+                      <AccountBalanceWalletOutlinedIcon
+                        fontSize={matchesSM ? "small" : "default"}
+                        color={
+                          theme.palette.type === "dark"
+                            ? "secondary"
+                            : "primary"
+                        }
+                      />
+                    </ListItemIcon>
+                    <ListItemText
+                      classes={{ root: classes.listItemText }}
+                      primary={matchesSM ? "Amount" : "Transaction Amount"}
+                    />
+                  </Grid>
+                  <Grid>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      primary="200$"
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider />
 
-            <ListItem>
-              <ListItemIcon classes={{ root: classes.listIcon }}>
-                <CategoryIcon
-                  color={
-                    theme.palette.type === "dark" ? "secondary" : "primary"
-                  }
-                />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ root: classes.listItemTextHead }}
-                style={{ marginRight: "9.5em" }}
-                primary="Category"
-              />
-              <ListItemText primary="Entertainment" />
-            </ListItem>
-            <Divider />
+              <ListItem>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item style={{ display: "inherit" }}>
+                    <ListItemIcon classes={{ root: classes.listIcon }}>
+                      <PersonOutlineOutlinedIcon
+                        fontSize={matchesSM ? "small" : "default"}
+                        color={
+                          theme.palette.type === "dark"
+                            ? "secondary"
+                            : "primary"
+                        }
+                      />
+                    </ListItemIcon>
+                    <ListItemText
+                      classes={{ root: classes.listItemText }}
+                      primary="Paid by"
+                    />
+                  </Grid>
+                  <Grid>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      primary="You"
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider />
 
-            <ListItem>
-              <ListItemIcon classes={{ root: classes.listIcon }}>
-                <DetailsIcon
-                  color={
-                    theme.palette.type === "dark" ? "secondary" : "primary"
-                  }
-                />
-              </ListItemIcon>
-              <ListItemText
-                classes={{ root: classes.listItemTextHead }}
-                style={{ marginRight: "11em" }}
-                primary="Details"
-              />
-              <ListItemText primary="Starwars movie" />
-            </ListItem>
-            <Divider />
-          </List>
-        </DialogContent>
+              <ListItem>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item style={{ display: "inherit" }}>
+                    <ListItemIcon classes={{ root: classes.listIcon }}>
+                      <CallSplitIcon
+                        fontSize={matchesSM ? "small" : "default"}
+                        color={
+                          theme.palette.type === "dark"
+                            ? "secondary"
+                            : "primary"
+                        }
+                      />
+                    </ListItemIcon>
+                    <ListItemText
+                      classes={{ root: classes.listItemText }}
+                      primary="Split type"
+                    />
+                  </Grid>
+                  <Grid>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      primary="Equal"
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider />
+
+              <ListItem>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item style={{ display: "inherit" }}>
+                    <ListItemIcon classes={{ root: classes.listIcon }}>
+                      <CategoryIcon
+                        fontSize={matchesSM ? "small" : "default"}
+                        color={
+                          theme.palette.type === "dark"
+                            ? "secondary"
+                            : "primary"
+                        }
+                      />
+                    </ListItemIcon>
+                    <ListItemText
+                      classes={{ root: classes.listItemText }}
+                      primary="Category"
+                    />
+                  </Grid>
+                  <Grid>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      primary="Entertainment"
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider />
+
+              <ListItem>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item style={{ display: "inherit" }}>
+                    <ListItemIcon classes={{ root: classes.listIcon }}>
+                      <DetailsIcon
+                        fontSize={matchesSM ? "small" : "default"}
+                        color={
+                          theme.palette.type === "dark"
+                            ? "secondary"
+                            : "primary"
+                        }
+                      />
+                    </ListItemIcon>
+                    <ListItemText
+                      classes={{ root: classes.listItemText }}
+                      primary="Details"
+                    />
+                  </Grid>
+                  <Grid>
+                    <ListItemText
+                      classes={{
+                        root: classes.listItemText,
+                      }}
+                      style={{ marginLeft: "39px" }}
+                      primary="Watched Starwars"
+                    />
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <Divider />
+            </List>
+          </DialogContent>
+        </Grid>
         <DialogActions className={classes.dialogActions}>
           <Button
             onClick={dialogCloseHandler}
             color={theme.palette.type === "light" ? "primary" : "secondary"}
+            size={matchesSM ? "small" : "medium"}
           >
             Edit
           </Button>
           <Button
             onClick={dialogCloseHandler}
             color={theme.palette.type === "light" ? "primary" : "secondary"}
+            size={matchesSM ? "small" : "medium"}
           >
             Cancel
           </Button>
